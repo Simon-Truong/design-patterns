8bcc340e5802c4ccb2708c514e7a9c4b55b44603
Simon Truong <simontruong2@gmail.com>
2019-07-11 08:45:37 +1000

    edited command design pattern

--- CommandDesignPattern/CommandDesignPattern/Program.cs
+++ CommandDesignPattern/CommandDesignPattern/Program.cs
@@ -11,10 +11,20 @@ namespace CommandDesignPattern
         static void Main(string[] args)
         {
             Television television = new Television();
-            TurnTvOn turnTvOnCommand = new TurnTvOn(television);
-            TvRemote turnOnTv = new TvRemote(turnTvOnCommand);
+            TurnTvOn turnOnTvCommand = new TurnTvOn(television);
+            TurnTvOff turnOffCommand = new TurnTvOff(television);
+            IncreaseTvVolume increaseTvVolumeCommand = new IncreaseTvVolume(television);
+            DecreaseTvVolume decreaseTvVolumeCommand = new DecreaseTvVolume(television);
+
+            TvRemote turnOnTv = new TvRemote(turnOnTvCommand);
+            TvRemote turnOffTv = new TvRemote(turnOffCommand);
+            TvRemote increaseTvVolume = new TvRemote(increaseTvVolumeCommand);
+            TvRemote decreaseTvVolume = new TvRemote(decreaseTvVolumeCommand);
 
             Console.WriteLine(turnOnTv.ExecuteCommand());
+            Console.WriteLine(turnOffTv.ExecuteCommand());
+            Console.WriteLine(increaseTvVolume.ExecuteCommand());
+            Console.WriteLine(decreaseTvVolume.ExecuteCommand());
             Console.ReadKey();
         }
     }
